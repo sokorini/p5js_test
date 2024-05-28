0a59bb045264105aaf5903e66d2ab25026a5cb78
SKRNI <67529745+sokorini@users.noreply.github.com>
2024-05-28 20:59:13 +0900

    Update functions.js

--- functions.js
+++ functions.js
@@ -6,7 +6,7 @@ function openSMS(ph, str){
     window.open('sms://'+ph+'/?body='+str);
   }
   if(navigator.userAgent.match(/iPad/i)){
-    window.open('sms://01052293829/?body='+str);
+    window.open('sms://01052293829/;body='+str);
   }
 }
 
