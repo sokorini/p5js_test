1a61a40c5efce2e2cc2c213c6b0f051787583393
SKRNI <67529745+sokorini@users.noreply.github.com>
2024-05-30 23:49:38 +0900

    Add files via upload

--- manageItems.js
+++ manageItems.js
@@ -2,19 +2,21 @@
 // integ : sha384-TiCUE00h649CAMonG018J2ujOgDKW/kVWlChEuu4jK2vxfAAD0eZxzCKakxg55G4
 
 let listView = null;
+let addItem;
 
 function setup() {
   fullscreen();
   createCanvas(windowWidth, windowHeight);
-
+  setAttributes({ antialias: false })
+  noStroke();
   background(220);
 
-  frameRate(30);
+  // frameRate(30);
 
   let saveAndGoBack = makeButton("저장하고 돌아가기", 0, 90, 100, 10);
   saveAndGoBack.mousePressed(saveAndGoBackCB);
 
-  let addItem = makeButton("추가하기", 0, 61, 100, 10);
+  addItem = makeButton("추가하기", 0, 61, 100, 10);
   addItem.mousePressed(addItemCB);
 
   listView = new ListView(0, HPerC(10), WPerC(100), HPerC(40));
@@ -30,11 +32,13 @@ function setup() {
   
   listView.draw();
   
+  
   let itemName = makeInput("", 0, 50, 49, 10);
   itemName.style('font-size', 20+'px');
   
   let itemUnit = makeInput("", 51, 50, 20, 10);
   itemUnit.style('font-size', 20+'px');
+
 }
 
 function saveAndGoBackCB(){
@@ -42,7 +46,7 @@ function saveAndGoBackCB(){
 }
 
 function addItemCB(){
-  
+  addItem.remove();
 }
 
 function draw(){
